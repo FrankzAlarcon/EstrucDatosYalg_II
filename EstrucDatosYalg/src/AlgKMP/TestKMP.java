3993da69fa8d4c7e8dec23a9906b81f52dae84bf
Frankz <Frankz@PC-Frankz>
2021-11-19 12:36:07 -0500

    KMP match algorithm

--- EstrucDatosYalg/src/AlgKMP/TestKMP.java
+++ EstrucDatosYalg/src/AlgKMP/TestKMP.java
@@ -5,6 +5,11 @@
  */
 package AlgKMP;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Arrays;
+
 /**
  *
  * @author Frankz
@@ -16,10 +21,74 @@ public class TestKMP {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        /*char []cadena = "gfjbfdgdfabcdfdfndfgjdfngudfabcdfdgfd".toCharArray();
+        char []pattern = "abcd".toCharArray();
+        System.out.println(findKMP(cadena, pattern));*/
+        long tiempoInicio = System.currentTimeMillis();
+        try{
+            File doc = new File("C:\\Users\\Frankz\\OneDrive\\Documentos\\NetBeansProjects\\EstrucDatosYalg_II\\EstrucDatosYalg\\src\\AlgKMP\\CadenasKMP.txt"); 
+            
+            BufferedReader obj = new BufferedReader(new FileReader(doc));
+            
+            String linea;
+            int cont=0;
+            while((linea = obj.readLine()) != null){
+                //System.out.println(linea);
+                cont++;
+                System.out.println("En la linea " + cont + ": " +findKMP(linea.toCharArray(), "0101001001010111".toCharArray()));
+            }
+        }catch(Exception e){
+             System.out.println(e.toString());
+        }
+        long tiempoFinal = System.currentTimeMillis() - tiempoInicio;
+        System.out.println("Tiempo utilizado (ms): " + tiempoFinal);
     }
     
-    private static int[] computeFailKMP() {
-        int []f=new int[1];
-        return f;
+    private static String findKMP(char[] text, char[] pattern){
+        int n = text.length;
+        int m = pattern.length;
+        String strLocations = "";
+        if(m == 0){
+            return strLocations;
+        }
+        int fail[] = computeFailKMP(pattern);
+        //System.out.println(Arrays.toString(fail));
+        int j = 0;
+        int k = 0;
+        while(j < n) {
+            if(text[j] == pattern[k]){
+                if(k==(m-1)){
+                    int pos = j-m+1;
+                    strLocations += pos + ", ";
+                    k = 0;
+                }
+                j++;
+                k++;
+            }else if(k>0){
+                k=fail[k-1];
+            } else {
+                j++;
+            }
+        }
+        return strLocations;
+    }
+    
+    private static int[] computeFailKMP(char[] patron) {
+        int longitudPatron = patron.length;
+        int []fail= new int[longitudPatron];
+        int runnerIndex = 1;
+        int mainIndex = 0;
+        while(runnerIndex < longitudPatron){
+            if(patron[mainIndex] == patron[runnerIndex]){
+                fail[runnerIndex] = mainIndex + 1;
+                mainIndex++;
+                runnerIndex++;
+            }else if(mainIndex > 0){
+                mainIndex = fail[mainIndex - 1];
+            } else {
+                runnerIndex++;
+            }
+        }
+        return fail;
     }
 }
